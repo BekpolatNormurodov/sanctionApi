b37485cef255366b78cb8af16582f6cf9af5b86c
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2024-09-18 12:27:24 +0500

    pdf url

--- api/serializers.py
+++ api/serializers.py
@@ -3,13 +3,15 @@ from api.models import *
 from config import settings
 
 class Testserializer(serializers.ModelSerializer):
+    pdf_url = serializers.SerializerMethodField(required=False)
+
     class Meta:
         model = Test
         fields = ('id','title', 'logo', 'pdf', 'pdf_url')
 
     def get_pdf_url(self, obj):
-        if obj.image:
-            pdf_url = f"{settings.MEDIA_URL}{obj.image}"
+        if obj.pdf:
+            pdf_url = f"{settings.MEDIA_URL}{obj.pdf}"
             pdf_fied_url = f"{settings.BASE_URL}/sanctionApi{pdf_url}"
             return pdf_fied_url
         return None
