1b7becca5c6bf1cd2789f69cc1376927161eb9b7
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2024-09-21 23:15:49 +0500

    create region, shakl1

--- api/models.py
+++ api/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Sanction(models.Model):
     title = models.CharField(max_length=100, null=True)
     date = models.DateField(default=date.today)
+    region = models.CharField(max_length=50, null=True)
+    shakl1 = models.CharField(max_length=10, null=True)
     pdf = models.FileField(upload_to='pdf/', null=True)
 
     def __str__(self):
--- api/serializers.py
+++ api/serializers.py
@@ -7,7 +7,7 @@ class Sanctionserializer(serializers.ModelSerializer):
 
     class Meta:
         model = Sanction
-        fields = ('id','title', 'date', 'pdf', 'pdf_url')
+        fields = ('id','title', 'date', 'region', 'shakl1', 'pdf', 'pdf_url')
 
     def get_pdf_url(self, obj):
         if obj.pdf:
