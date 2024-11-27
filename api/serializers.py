0054e1856ac4d38e306d9eeb6781d9e3269986f5
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2024-11-27 11:35:00 +0500

    env

--- api/serializers.py
+++ api/serializers.py
@@ -5,16 +5,24 @@ from config import settings
 # Oper
 class SanctionOperserializer(serializers.ModelSerializer):
     pdf_url = serializers.SerializerMethodField(required=False)
+    pdf_2_url = serializers.SerializerMethodField(required=False)
 
     class Meta:
         model = SanctionOper
-        fields = ('id', 'date', 'hackType', 'region', 'shakl1', 'pdf', 'pdf_url')
+        fields = ('id', 'date', 'hackType', 'region', 'shakl1', 'pdf', 'pdf_url', 'pdf_2', 'pdf_2_url')
 
     def get_pdf_url(self, obj):
         if obj.pdf:
             pdf_url = f"{settings.MEDIA_URL}{obj.pdf}"
             pdf_fied_url = f"{settings.BASE_URL}/sanctionApi{pdf_url}"
             return pdf_fied_url
+        return None
+    
+    def get_pdf_2_url(self, obj):
+        if obj.pdf:
+            pdf_2_url = f"{settings.MEDIA_URL}{obj.pdf}"
+            pdf_2_fied_url = f"{settings.BASE_URL}/sanctionApi{pdf_2_url}"
+            return pdf_2_fied_url
         return None
     
 
@@ -23,10 +31,11 @@ class SanctionOperserializer(serializers.ModelSerializer):
 # IIB
 class SanctionIIBserializer(serializers.ModelSerializer):
     pdf_url = serializers.SerializerMethodField(required=False)
+    pdf_2_url = serializers.SerializerMethodField(required=False)
 
     class Meta:
         model = SanctionIIB
-        fields = ('id', 'date', 'hackType', 'region', 'shakl1', 'pdf', 'pdf_url')
+        fields = ('id', 'date', 'hackType', 'region', 'shakl1', 'pdf', 'pdf_url', 'pdf_2', 'pdf_2_url')
 
     def get_pdf_url(self, obj):
         if obj.pdf:
@@ -34,22 +43,35 @@ class SanctionIIBserializer(serializers.ModelSerializer):
             pdf_fied_url = f"{settings.BASE_URL}/sanctionApi{pdf_url}"
             return pdf_fied_url
         return None
+    def get_pdf_2_url(self, obj):
+        if obj.pdf:
+            pdf_2_url = f"{settings.MEDIA_URL}{obj.pdf}"
+            pdf_2_fied_url = f"{settings.BASE_URL}/sanctionApi{pdf_2_url}"
+            return pdf_2_fied_url
+        return None
     
 
 
 # Prokuratura
 class SanctionProkuraturaserializer(serializers.ModelSerializer):
     pdf_url = serializers.SerializerMethodField(required=False)
+    pdf_2_url = serializers.SerializerMethodField(required=False)
 
     class Meta:
         model = SanctionProkuratura
-        fields = ('id', 'date', 'hackType', 'region', 'shakl1', 'pdf', 'pdf_url')
-
+        fields = ('id', 'date', 'hackType', 'region', 'shakl1', 'pdf', 'pdf_url', 'pdf_2', 'pdf_2_url')
+        
     def get_pdf_url(self, obj):
         if obj.pdf:
             pdf_url = f"{settings.MEDIA_URL}{obj.pdf}"
             pdf_fied_url = f"{settings.BASE_URL}/sanctionApi{pdf_url}"
             return pdf_fied_url
+        return None
+    def get_pdf_2_url(self, obj):
+        if obj.pdf:
+            pdf_2_url = f"{settings.MEDIA_URL}{obj.pdf}"
+            pdf_2_fied_url = f"{settings.BASE_URL}/sanctionApi{pdf_2_url}"
+            return pdf_2_fied_url
         return None
     
 
